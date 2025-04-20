883c094ec9262d266dec9c4af0e2f7402888d949
Tomas Mejia <tomasmejiadiaz1999@gmail.com>
2025-04-19 19:29:07 -0500

    creacion de server con ciertos parametros a tener en cuenta para el servidor

--- server/app.js
+++ server/app.js
@@ -4,6 +4,11 @@ const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+app.use(cors());
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
