b196f0ac17e478336dcee89897bc7556133c3986
Tomas Mejia <tomasmejiadiaz1999@gmail.com>
2025-04-20 00:23:44 -0500

    se agrega nodemon y database mas otras configuraciones

--- server/app.js
+++ server/app.js
@@ -3,16 +3,22 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
+const db = require('./config/db.js');
 
 app.use(cors());
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 5000;
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  db.query('SELECT NOW()', (err, results) => {
+    if (err) return res.status(500).send('Error al conectar con la BD');
+    res.send('Servidor conectado a MySQL. Hora actual: ' + results[0]['NOW()']);
+  });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
 });
