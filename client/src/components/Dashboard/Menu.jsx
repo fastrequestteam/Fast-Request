342768500ca914bf5adf3e876aeb4c0a708472d0
Tomas Mejia <tomasmejiadiaz1999@gmail.com>
2025-10-26 17:42:35 -0500

    avance 26 octubre

--- client/src/components/Dashboard/Menu.jsx
+++ client/src/components/Dashboard/Menu.jsx
@@ -4,16 +4,18 @@ import useConfiguracionEmpresa from '../../hooks/useConfiguracionEmpresa';
 
 function MenuDashboard() {
     const [activeIndex, setActiveIndex] = useState(null);
+    const location = useLocation();
 
     const handleMouseOver = (index) => {
         setActiveIndex(index);
     };
 
     const initial = {
-        NombreEmpresa: ''
+        NombreEmpresa: '',
+        Imagen_De_Perfil: "http://localhost:5000/uploads/perfiles/user.png",
     }
 
-    const { userData, cargarEmpresa } = useConfiguracionEmpresa(initial)
+    const { userData, cargarEmpresa, API_GET } = useConfiguracionEmpresa(initial)
 
     const { NombreEmpresa } = userData;
 
@@ -29,11 +31,27 @@ function MenuDashboard() {
     }, [cargarEmpresa])
 
 
+  useEffect(() => {
+    const handleProfileUpdate = () => {
+      API_GET();
+    };
+
+    window.addEventListener("userProfileUpdated", handleProfileUpdate);
+    return () =>
+      window.removeEventListener("userProfileUpdated", handleProfileUpdate);
+  }, [API_GET]);
+
+
     return (
         <div className="navdash">
             <ul className='nav-ul'>
                 {[
-                    { icon: 'paper-plane', title: NombreEmpresa.toUpperCase() },
+                    { 
+                        imageProfile: userData.Imagen_De_Perfil instanceof File
+                            ? URL.createObjectURL(userData.Imagen_De_Perfil)
+                            : userData.Imagen_De_Perfil || "https://res.cloudinary.com/dp9jbvpwl/image/upload/v1757260230/user_izbzpi.png",
+                        title: NombreEmpresa.toUpperCase() 
+                    },
                     { to: '/dashboard', icon: 'home', title: 'Inicio' },
                     { to: '/dashboard/hacerPedido', icon: 'create', title: 'Hacer Pedido' },
                     { to: '/dashboard/pedidos', icon: 'receipt', title: 'Pedidos' },
@@ -58,7 +76,20 @@ function MenuDashboard() {
                             </NavLink>
                         ) : (
                             <span className='nav-ul-li-a no-link'>
-                                <span className="icono"><ion-icon name={item.icon}></ion-icon></span>
+                                {item.imageProfile ? (
+                                    <span className="icono">
+                                        <img 
+                                            src={item.imageProfile} 
+                                            alt="Avatar" 
+                                            className="nav-avatar object-cover" 
+                                            style={{ width: '50px', height: '50px' }}
+                                        />
+                                    </span>
+                                ) : (
+                                    <span className="icono">
+                                        <ion-icon name={item.icon}></ion-icon>
+                                    </span>
+                                )}
                                 <span className="nav-titulo-name">{item.title}</span>
                             </span>
                         )}
